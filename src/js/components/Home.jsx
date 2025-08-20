d08e96d458e165186d4c7790bc0d7f460869960b
Vlad <pacom4n90@gmail.com>
2025-08-20 19:12:14 +0000

    TodoList Vlad

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,14 +1,12 @@
 import React, { useEffect } from 'react';
 import TodoList from './TodoList'; // Importa el componente TodoList
-import { getUsers } from './services/SERVICIOS';
+
 
 
 
 
 function App() {
-  useEffect (()=>{
-    getUsers()
-  },[])
+  
   return (
     <div className="App">
       
